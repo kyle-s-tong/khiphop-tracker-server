e79b914bd41aff369b70307e5d55b7883e92d788
Kyle <kyle.simon.tong@gmail.com>
2020-04-12 11:55:19 +1200

    Allow cors from other local host port for integration

--- src/index.js
+++ src/index.js
@@ -6,6 +6,12 @@ import router from './router';
 
 const app = express();
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:4200"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(bodyParser.json());
 
 app.use(router);
